1e26691e644c332d03fd47159ff3354b07590b0e
Adam Florin <adamflorin@gmail.com>
2014-03-08 14:17:30 -0800

    repoint imports to 'adamflorin'

--- trackerapi/me.go
+++ trackerapi/me.go
@@ -8,8 +8,8 @@ import (
 	"os"
 	u "os/user"
 
-	"github.com/GoBootcamp/clirescue/cmdutil"
-	"github.com/GoBootcamp/clirescue/user"
+	"github.com/adamflorin/clirescue/cmdutil"
+	"github.com/adamflorin/clirescue/user"
 )
 
 var (
